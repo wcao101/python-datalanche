31c28d8bf4dbb22d3a9d77daf56e2b36f243210c
rpedela <rpedela@datalanche.com>
2013-03-14 16:44:35 -0600

    Add license and packaging setup file.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+required = ['requests == 1.1', 'enum == 0.4.4']
+
+setup(
+    author='Datalanche, Inc.',
+    name='datalanche',
+    version='0.0.2',
+    description='Python client for Datalanche\'s REST API.',
+    long_description=open('README.md').read(),
+    url='https://github.com/datalanche/python-datalanche',
+    license=open('LICENSE').read(),
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    keywords=['datalanche'],
+    packages=find_packages(),
+    install_requires=required
+)
