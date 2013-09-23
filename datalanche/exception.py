1abbe4efac17ab1309ac08c57ab3aa4ab1f1260b
wcao101 <weiqiang.cao@colorado.edu>
2013-09-23 15:32:52 -0600

    modified the parameters in DLException and the members in the DLException class

--- datalanche/exception.py
+++ datalanche/exception.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
 class DLException(Exception):
-    def __init__(self, status_code, response, debug_info):
-        self.response = {}
-        self.status_code = status_code
+    def __init__(self, status_code, body, debug_info):
+        self.response = debug_info['response']
         self.response['body'] = debug_info['request']['body']
         del debug_info['request']['body'] # the ['body'] should be in response
-        self.response['http_status'] = debug_info['response']['http_status']
-        self.response['headers'] = debug_info['response']['headers']
         self.request = debug_info['request']
-        self.error_message = response['message']
-        self.error_type = response['code']
+        self.error_message = body['message']
+        self.error_type = body['code']
+        self.status_code = status_code
+
         exception = {
-            'status_code': status_code,
+            'status_code': self.status_code,
             'error_message' : self.error_message,
             'error_type' : self.error_type,
             'request': self.request,
