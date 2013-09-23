df78ff33c0caa101ad565e3af3b61d8d7189d012
wcao101 <weiqiang.cao@colorado.edu>
2013-09-23 15:00:09 -0600

    #8, move the 'body' object to response from request.

--- datalanche/exception.py
+++ datalanche/exception.py
@@ -5,6 +5,7 @@ class DLException(Exception):
         self.response = {}
         self.status_code = status_code
         self.response['body'] = debug_info['request']['body']
+        del debug_info['request']['body'] # the ['body'] should be in response
         self.response['http_status'] = debug_info['response']['http_status']
         self.response['headers'] = debug_info['response']['headers']
         self.request = debug_info['request']
@@ -12,8 +13,10 @@ class DLException(Exception):
         self.error_type = response['code']
         exception = {
             'status_code': status_code,
-            'error_message' : response['message'],
-            'error_type' : response['code']
+            'error_message' : self.error_message,
+            'error_type' : self.error_type,
+            'request': self.request,
+            'response': self.response
         }
         Exception.__init__(self, exception)
 
