0d70b8701071224ebb906c53eefdd151fcfa5418
wcao101 <weiqiang.cao@colorado.edu>
2013-09-23 15:58:11 -0600

    clean up imports

--- datalanche/client.py
+++ datalanche/client.py
@@ -6,30 +6,44 @@ import collections
 import urllib
 from StringIO import StringIO
 from exception import DLException
-from collections import OrderedDict
 from requests.auth import HTTPBasicAuth
 
+
 def get_body(query=None):
-    return query.params
+    body = {}
+    if(query == None):
+        return body
+    else:
+        for keys,values in query.params.items():
+            body[keys] = values
+    return body
    
-# For POST, all parameters are in the body so that they are also encrypted.  
+# For POST, all parameters are in the body so that they are also encrypted.
 # For example, the WHERE clause may have unique identifiers, plain-text
 # passwords, and other potentially sensitive information.
 def get_url(query=None):
     if (query == None):
         return '/'
-    url = query.url
-    parameters = OrderedDict()
-    query_str = None
-    if (url == '/drop_table' or url == '/get_table_info'):
-        query_str = urllib.urlencode(query.params['table_name'])
-    if (query_str != None):
+    url = query.base_url
+    parameters = collections.OrderedDict()
+    if (url == '/drop_table'):
+        if (query.params['table_name'] != None):
+            parameters['table_name'] = query.params['table_name']
+    elif (url == '/get_table_info'):
+        if (query.params['table_name'] != None):
+            parameters['table_name'] = query.params['table_name']
+    elif (url == '/get_table_list'):
+        pass
+        # do nothing
+    query_str = urllib.urlencode(parameters)
+    if (query_str != ''):
         url += '?' + query_str
     return url
 
 class DLClient(object):
-    def __init__(self, key='', secret='', 
-                 host=None, port=None, verify_ssl=True):
+    def __init__(
+            self, key='', secret='', 
+            host=None, port=None, verify_ssl=True):
         self.auth_key = key
         self.auth_secret = secret
         self.client = requests.Session()
@@ -46,7 +60,7 @@ class DLClient(object):
     def secret (self, secret=None):
         self.auth_secret = secret
 
-    def get_debug_info(self,r):
+    def get_debug_info(self, r):
         info_obj = collections.OrderedDict()
         info_obj ['request']= collections.OrderedDict()
         info_obj ['response'] = collections.OrderedDict()
@@ -58,28 +72,26 @@ class DLClient(object):
         info_obj ['response']['headers'] = r.headers
         return info_obj
         
-    def query(self, q=None):
+    def query(self,q=None):
         if (q == None):
-            print "Error: query is None."
+            raise Exception("Error: query is None!")
             return None
-        if not q.url_type:
+        if (q.url_type != ''):
             r = self.client.post(
                 url=self.url + get_url(q),
                 auth=HTTPBasicAuth(self.auth_key, self.auth_secret),
                 headers={'Content-type':'application/json'},
                 data=json.dumps(get_body(q)),
-                verify=self.verify_ssl
-            )
+                verify=self.verify_ssl)
             result = {}
             debug_info = self.get_debug_info(r)
-            if (q.url == '/select_from' or q.url_type == 'get'):
+            if (q.base_url == '/select_from' or q.url_type == 'get'):
                 result['data'] = r.json(
-                    object_pairs_hook=collections.OrderedDict
-                )            
+                    object_pairs_hook=collections.OrderedDict)            
             result['response'] = debug_info['response']
             result['request'] = debug_info['request']
             if not 200 <= r.status_code < 300:
                 raise DLException(r.status_code, r.json(), debug_info)
             return result
         else:
-            print "Error: unsupported query type"
+            raise Exception("Error: unsupported query type!")
