f7eb915bbcca5ba4bb5d835126362208e090a6c8
wcao101 <weiqiang.cao@colorado.edu>
2013-09-23 14:58:06 -0600

    #8, add fix the debug info for the DLException

--- datalanche/exception.py
+++ datalanche/exception.py
@@ -2,23 +2,16 @@
 
 class DLException(Exception):
     def __init__(self, status_code, response, debug_info):
+        self.response = {}
         self.status_code = status_code
-        self.response = response
-        self.request_info = debug_info['request']
-        self.response_info = debug_info['response']
+        self.response['body'] = debug_info['request']['body']
+        self.response['http_status'] = debug_info['response']['http_status']
+        self.response['headers'] = debug_info['response']['headers']
+        self.request = debug_info['request']
         self.error_message = response['message']
         self.error_type = response['code']
-        self.info = {
-            'status_code': status_code,
-            'request_info' : debug_info['request'],
-            'response_info' : debug_info['response'],
-            'error_message' : response['message'],
-            'error_type' : response['code']
-        }
         exception = {
             'status_code': status_code,
-            'request_info' : debug_info['request'],
-            'response_info' : debug_info['response'],
             'error_message' : response['message'],
             'error_type' : response['code']
         }
