fe9f14502635240743ba7b3970506eaf4fd70c1a
wcao101 <weiqiang.cao@colorado.edu>
2014-02-10 15:24:58 -0700

    remove the uncessary code for setting client

--- examples/schema/create_schema.py
+++ examples/schema/create_schema.py
@@ -14,26 +14,17 @@ try:
 
     # Load config.json for setting API_KEY, API_SECRET, host, port and verify_ssh
     # change the settings in config.json before running examples
-    config_file = "../../config.json"
-    config = json.load(open(config_file), object_pairs_hook=collections.OrderedDict)
+    config = json.load(open("../../config.json"))
 
-    config['verify_ssl'] = config['verify_ssl'].lower()
-    if config['verify_ssl'] == '0' or config['verify_ssl'] == 'false':
-        config['verify_ssl'] = False
-    else:
-        config['verify_ssl'] = True
+    # Please change YOUR_API_KEY and YOUR_API_SECRET before use
+    YOUR_API_KEY = config['api_key']
+    YOUR_API_SECRET = config['api_secret']
+    
+    client = DLClient(key = YOUR_API_KEY, secret = YOUR_API_SECRET)
 
-    client = DLClient(key = config['api_key'], 
-                      secret = config['api_secret'], 
-                      host = config['host'], 
-                      port = config['port'], 
-                      verify_ssl = config['verify_ssl'])
-
-    # or you can also set your API_KEY and API_SECRET to python client like this
-    # client = DLClient(key='YOUR_API_KEY', secret='YOUR_API_SECRET')
 
     q = DLQuery(database = 'my_database')
-    q.create_schema('my_schema_1')
+    q.create_schema('my_schema')
     q.description('my_schema description text')
 
     client.query(q)
