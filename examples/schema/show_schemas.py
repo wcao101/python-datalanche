468625ed2c368590d2f19061ad79d719ba9b94c4
wcao101 <weiqiang.cao@colorado.edu>
2014-02-12 15:34:15 -0700

    correct the config.json's path for the rest files

--- examples/schema/show_schemas.py
+++ examples/schema/show_schemas.py
@@ -8,7 +8,7 @@ import sys
 
 try:
 
-    config = json.load(open("../../contest.json"))
+    config = json.load(open("../contest.json"))
 
     # Please find your API credentials here: https://www.datalanche.com/account before use            
     YOUR_API_KEY = config['api_key']
